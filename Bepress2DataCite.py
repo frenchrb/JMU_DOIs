0c1671dcfbd96af0b9faaf816605868ad4ecd0ba
Rebecca French <frenchrb@jmu.edu>
2019-04-04 08:47:38 -0400

    Move all draft script changes to production scripts

--- Bepress2DataCite.py
+++ Bepress2DataCite.py
@@ -1,39 +1,81 @@
 import argparse
 import configparser
-import sys
+import os
+import re
 import subprocess
+import sys
+from datetime import datetime
+from lxml import etree
 from pathlib import Path
 
-#input is path to Bepress spreadsheet saved as "XML Spreadsheet 2003"
+
 def main(arglist):
     #print(arglist)
     parser = argparse.ArgumentParser()
+    parser.add_argument('setname', help='bepress collection setname (e.g., diss201019)')
     parser.add_argument('input', help='path to Bepress spreadsheet saved as "XML Spreadsheet 2003"')
     #parser.add_argument('output', help='save directory')
     #parser.add_argument('--production', help='production DOIs', action='store_true')
     args = parser.parse_args(arglist)
     #print(args)
     
-    #read config file
-    config = configparser.ConfigParser()
+    # Read config file and parse setnames into lists by category
+    config = configparser.ConfigParser(allow_no_value=True)
     config.read('local_settings.ini')
+    etd_setnames = []
+    for i in config.items('ETD'):
+        etd_setnames.append(i[0])
+    # Add additional categories here
     
-    input = Path(arglist[0])
+    setname = arglist[0]
+    input = Path(arglist[1])
     
-    xsl_excel_named = 'coll_transforms/Excel2NamedXML.xsl'
-    xsl_coll_transform = 'coll_transforms/ExcelNamed2DataCite_dnp201019.xsl'
-
+    # Timestamp output
+    date_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print(date_time)
+    print()
+    print('------------------------------------------------------------')
+    print('------------------------------------------------------------')
+    
+    temp_file = Path('excel_named_temp.xml')
+    # Remove temp_file if it already exists
+    if temp_file.is_file():
+        os.remove(str(temp_file))
+    
+    xsl_excel_named = Path('coll_transforms/Excel2NamedXML.xsl')
+    if setname in etd_setnames:
+        xsl_coll_transform = Path('coll_transforms/ExcelNamed2DataCite_etd.xsl')
+    # Add additional categories here
+    else:
+        xsl_coll_transform = Path('coll_transforms/ExcelNamed2DataCite_' + setname + '.xsl')
+    
     # Transform Excel XML into XML with named nodes
     print('Transforming Excel XML...')
-    subprocess.call(['java', '-jar', config['Saxon']['saxon_path']+'saxon9he.jar', '-s:'+str(input), '-xsl:'+xsl_excel_named, '-o:excel_named_temp.xml', '-versionmsg:off'])
+    subprocess.call(['java', '-jar', config['Saxon']['saxon_path']+'saxon9he.jar', '-s:'+str(input), '-xsl:'+str(xsl_excel_named), '-o:'+str(temp_file), '-versionmsg:off'])
     print('Transformation complete')
     print()
     
-    # Transform Excel Named XML to DataCite XML (items without DOIs only)
-    # Output location and filenames are specified in XSLT
-    print('Transforming Excel to DataCite XML...')
-    subprocess.call(['java', '-jar', config['Saxon']['saxon_path']+'saxon9he.jar', '-s:excel_named_temp.xml', '-xsl:'+xsl_coll_transform])
-    print('Transformation complete')
+    # Get URL from bepress spreadsheet
+    tree = etree.parse(str(temp_file))
+    # url = tree.xpath('/table/row/calc_url/text()')
+    # url_setname = re.sub(r'^http:\/\/commons.lib.jmu.edu\/(.*)\/\d*$', r'\g<1>', url[0])
+    url_setname = tree.xpath('/table/row/issue/text()')[0]
     
+    # Check that stylesheet exists and setname input matches setname in metadata before doing transformation
+    if not xsl_coll_transform.is_file():
+        print('Stylesheet ' + str(xsl_coll_transform) + ' does not exist')
+    elif not setname == url_setname:
+        print('Provided setname does not match setname in bepress spreadsheet')
+    else:
+        # Transform Excel Named XML to DataCite XML (items without DOIs only)
+        # Output location and filenames are specified in XSLT
+        print('Transforming Excel to DataCite XML...')
+        subprocess.call(['java', '-jar', config['Saxon']['saxon_path']+'saxon9he.jar', '-s:'+str(temp_file), '-xsl:'+str(xsl_coll_transform)])
+        print('Transformation complete')
+    print('------------------------------------------------------------')
+    print('------------------------------------------------------------')
+    print()
+
+
 if __name__ == '__main__':
     main(sys.argv[1:])
--- MintDOIs.py
+++ MintDOIs.py
@@ -1,14 +1,15 @@
 import argparse
+import configparser
+import re
+import requests
 import sys
 import xlrd
 import xlwt
 import xlutils.copy
-import re
-import configparser
-import requests
+from datetime import datetime
+from lxml import etree
 from pathlib import Path
 
-#input is path to bepress spreadsheet in "Excel 97-2003 Workbook (.xls)" format
 def main(arglist):
     #print(arglist)
     parser = argparse.ArgumentParser()
@@ -18,30 +19,23 @@ def main(arglist):
     args = parser.parse_args(arglist)
     #print(args)
     
-    #read config file with API endpoint, username, and password
-    config = configparser.ConfigParser()
+    # Read config file and parse setnames into lists by category
+    config = configparser.ConfigParser(allow_no_value=True)
     config.read('local_settings.ini')
+    etd_setnames = []
+    for i in config.items('ETD'):
+        etd_setnames.append(i[0])
+    #Add additional categories here
     
     input = Path(arglist[0])
     
-    #upload DataCite metadata containing DOI
-    #sends all .xml in the specified directory
-    print('Uploading metadata to DataCite...')
-    metadata_dir = Path.cwd() / 'DataCite_metadata'
-    for filename in metadata_dir.iterdir():
-        if filename.suffix == '.xml':
-            print('Sending metadata to DataCite:',filename)
-            metadata = (metadata_dir / filename).open('r', encoding='utf-8').read()
-            response = requests.post(config['DataCite API']['endpoint_md'], auth = (config['DataCite API']['username'],config['DataCite API']['password']), data = metadata.encode('utf-8'), headers = {'Content-Type':'application/xml;charset=UTF-8'})
-            if response.status_code == 201:
-                print(response.text)
-            else:
-                print(str(response.status_code) + ' ' + response.text)
-                
-    print('Metadata upload complete')
-    print()
+    # Timestamp output
+    date_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print(date_time)
     
-    #read Bepress spreadsheet
+    # Read Bepress spreadsheet
+    print()
+    print('Reading spreadsheet...')
     book_in = xlrd.open_workbook(str(input))
     sheet1 = book_in.sheet_by_index(0) #get first sheet
     sheet1_name = book_in.sheet_names()[0] #name of first sheet
@@ -52,23 +46,27 @@ def main(arglist):
     try:
         doi_col_index = sheet1_col_headers.index('doi')
     except ValueError:
-        print('DOI field not found in Bepress metadata')
-
-    url_col_index = sheet1_col_headers.index('calc_url')
-    pub_date_col_index = sheet1_col_headers.index('publication_date')
+        print('DOI field not found in bepress metadata')
     
-    #turn the xlrd Book into xlwt Workbook
+    url_col_index = sheet1_col_headers.index('calc_url')
+    #pub_date_col_index = sheet1_col_headers.index('publication_date')
+    
+    # Turn the xlrd Book into xlwt Workbook
     book_out = xlutils.copy.copy(book_in)
+    out_filename = input.stem + '_with_DOIs.xls'
+    out_path = input.parent / out_filename
     
     doi_col_values = sheet1.col_values(doi_col_index) #includes header row
     #print(doi_col_values)
     
-    #get number of blank cells in DOIs column
+    # Get number of blank cells in DOIs column
     doi_count = doi_col_values[1:].count('')
     print(doi_count,'DOIs will be created')
     print()
+    print('------------------------------------------------------------')
+    print('------------------------------------------------------------')
     
-    #create list of row indices of new DOIs to be created
+    # Create list of row indices of new DOIs to be created
     new_dois = []
     #print(list(zip(range(len(doi_col_values)), doi_col_values)))
     for (x,y) in zip(range(len(doi_col_values)), doi_col_values):
@@ -77,45 +75,92 @@ def main(arglist):
             new_dois.append(x)
     #print(new_dois)
     
-    #construct DOI for items without one and insert in spreadsheet
+    metadata_dir = Path.cwd() / 'DataCite_metadata'
+    
+    # Iterate over spreadsheet rows with no DOI
     for row in range(1,sheet1.nrows):
         if row in new_dois:
             url = sheet1.cell(row,url_col_index).value
-            set_name = re.sub(r'http:\/\/commons\.lib\.jmu\.edu\/(.*?)\/(.*)$', r'\g<1>', url, flags=re.S)
-            item_number = re.sub(r'http:\/\/commons\.lib\.jmu\.edu\/(.*?)\/(.*)$', r'\g<2>', url, flags=re.S)
-        
-            #get pub_year from Excel date format
-            #excel_date_number = sheet1.cell(row,pub_date_col_index).value
-            #pub_year, month, day, hour, minute, second = xlrd.xldate_as_tuple(excel_date_number, book_in.datemode)
-        
-            doi = '10.25885/etd/' + set_name + '/' + item_number
-        
-            print('Sheet row #',row+1)
+            set_name = re.sub(r'https:\/\/commons\.lib\.jmu\.edu\/(.*?)\/(.*)$', r'\g<1>', url, flags=re.S)
+            item_number = re.sub(r'https:\/\/commons\.lib\.jmu\.edu\/(.*?)\/(.*)$', r'\g<2>', url, flags=re.S)
+            
+            # Get pub_year from Excel date format
+            # excel_date_number = sheet1.cell(row,pub_date_col_index).value
+            # pub_year, month, day, hour, minute, second = xlrd.xldate_as_tuple(excel_date_number, book_in.datemode)
+            
+            print('Row',row+1)
             print(sheet1.cell(row, 0).value) #title
-            print('URL:',url)
-            #print('Current DOI in sheet:',sheet1.cell(row, doi_col_index).value) #value in doi column
-            print('DOI:',doi)
-            #print()
+            print('  URL:               ' + url)
+            # print('Current DOI in sheet:',sheet1.cell(row, doi_col_index).value) #value in doi column
+            # Construct DOI
+            doi = '10.25885/'
+            if set_name in etd_setnames:
+                doi += 'etd/'
+            # Add additional categories here
+            doi += set_name + '/' + item_number
+            print('  DOI will be:       ' + doi)
+            # print()
             
-            doi_param = 'doi='+doi
-            url_param = 'url='+url
-            doi_uri = doi_param + '\n' + url_param
-            #print (doi_uri)
-    
-            response2 = requests.put(config['DataCite API']['endpoint_doi'] + '/' + doi, auth = (config['DataCite API']['username'], config['DataCite API']['password']), data = doi_uri, headers = {'Content-Type':'text/plain;charset=UTF-8'})
-            #response2 = requests.put(config['DataCite API']['endpoint_doi'] + '/' + doi, auth = (config['DataCite API']['username'], config['DataCite API']['password']), data = doi_uri, headers = {'Content-Type':'text/plain;charset=UTF-8'})
-            if response2.status_code == 201:
-                print(response2.text)
-                #write DOI to sheet
-                book_out.get_sheet(0).write(row,doi_col_index,'https://doi.org/'+doi)
-            else:
-                print(str(response2.status_code) + ' ' + response2.text)
+            metadata_filename = ''
+            if set_name in etd_setnames:
+                metadata_filename += 'etd_'
+            metadata_filename += set_name + '_' + item_number + '.xml'
+            metadata_file = metadata_dir / metadata_filename
+            
+            # Verify that metadata file exists
+            if metadata_file in metadata_dir.iterdir():
+                # Get DOI from metadata file
+                tree = etree.parse(str(metadata_file))
+                metadata_doi = tree.xpath('/dcite:resource/dcite:identifier[@identifierType="DOI"]/text()', namespaces={'dcite' : 'http://datacite.org/schema/kernel-4'})[0]
+                print('  DOI from metadata: ' + metadata_doi)
                 
+                # Verify that constructed DOI matches DOI in metadata
+                if doi == metadata_doi:
+                    print('DOI match verified')
+                    
+                    # Upload DataCite metadata
+                    print()
+                    print('Uploading metadata (' + metadata_filename + ') to DataCite...')
+                    metadata = metadata_file.open('r', encoding='utf-8').read()
+                    response = requests.post(config['DataCite API']['endpoint_md'], auth = (config['DataCite API']['username'],config['DataCite API']['password']), data = metadata.encode('utf-8'), headers = {'Content-Type':'application/xml;charset=UTF-8'})
+                    if response.status_code == 201:
+                        print('  ' + response.text)
+                        print('Metadata upload successful')
+                        
+                        # Send DOI/URL to DataCite to mint DOI
+                        print()
+                        print('Minting DOI...')
+                        doi_param = 'doi='+doi
+                        url_param = 'url='+url
+                        doi_url = doi_param + '\n' + url_param
+                        # print (doi_url)
+                        
+                        response2 = requests.put(config['DataCite API']['endpoint_doi'] + '/' + doi, auth = (config['DataCite API']['username'], config['DataCite API']['password']), data = doi_url, headers = {'Content-Type':'text/plain;charset=UTF-8'})
+                        # response2 = requests.put(config['DataCite API']['endpoint_doi'] + '/' + doi, auth = (config['DataCite API']['username'], config['DataCite API']['password']), data = doi_url, headers = {'Content-Type':'text/plain;charset=UTF-8'})
+                        if response2.status_code == 201:
+                            print('  ' + response2.text)
+                            print('DOI created')
+                            
+                            # Write DOI to sheet and save in same place as original input spreadsheet
+                            book_out.get_sheet(0).write(row,doi_col_index,'https://doi.org/'+doi)
+                            book_out.save(str(out_path))
+                        else:
+                            print('  ' + str(response2.status_code) + ' ' + response2.text)
+                            print('DOI not created')
+                        
+                    else:
+                        print('  ' + str(response.status_code) + ' ' + response.text)
+                        print('Metadata upload failed')
+                else:
+                    print('DOIs do not match; check metadata')
+            else:
+                print('Metadata file not found')
             print()
-            
-    #spreadsheet with DOIs added saved in same place as original input spreadsheet
-    out_filename = input.stem + '_DOIs_added.xls'
-    out_path = input.parent / out_filename
+            print('------------------------------------------------------------')
+    
+    print('------------------------------------------------------------')
+    
+    # Spreadsheet with DOIs added saved in same place as original input spreadsheet
     book_out.save(str(out_path))
     print('Spreadsheet updated with DOIs')
     print('Saved at ' + str(out_path))
